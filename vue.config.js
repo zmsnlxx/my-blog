fb1372745a583ef301368db3e92a7f0a3b79fe14
liuxiang <liuxiang@aiyunxiao.com>
2019-12-13 14:31:46 +0800

    update: 更新页面排版

--- vue.config.js
+++ vue.config.js
@@ -1,47 +1,6 @@
-const path = require('path');
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/blog" : "/",
-  // 输出文件目录
-  outputDir: 'dist',
-  // eslint-loader 是否在保存的时候检查
-  lintOnSave: true,
-
-  configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境修改配置...
-      config.mode = 'production';
-    } else {
-      // 为开发环境修改配置...
-      config.mode = 'development';
-    }
-  },
-  // 生产环境是否生成 sourceMap 文件
-  productionSourceMap: true,
-  // css相关配置
-  css: {
-    // 是否使用css分离插件 ExtractTextPlugin
-    extract: true,
-    // 开启 CSS source maps?
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {},
-    // 启用 CSS modules for all css / pre-processor files.
-    modules: false
-  },
-  // use thread-loader for babel & TS in production build
-  // enabled by default if the machine has more than 1 cores
-  parallel: require('os').cpus().length > 1,
-  // PWA 插件相关配置
-  // see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
-  pwa: {},
-  // webpack-dev-server 相关配置
+  lintOnSave: false,
   devServer: {
-    open: process.platform === 'darwin',
-    host: '0.0.0.0',
-    port: 8081,
-    https: false,
-    hotOnly: false,
     proxy: {
       "/api": {
         target: "http://localhost:3000/api/",
@@ -51,5 +10,5 @@ module.exports = {
         }
       },
     }
-  },
+  }
 };
