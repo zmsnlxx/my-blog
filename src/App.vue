60cbc3e3be0cff4d626896d2a1dd14e21b685d39
liuxiang <zmsnlxx@163.com>
2020-12-11 14:18:18 +0800

    add: 新增备案链接跳转

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <div id="app">
         <header-nav class="header"/>
         <router-view style="padding-bottom: 100px"/>
-        <el-footer style="background-color: #475669;text-align: center;color: white;line-height: 60px">Copyright© 2019-2020 版权所有 zmsnlxx 豫ICP备19042595号-1</el-footer>
+        <el-footer style="background-color: #475669;text-align: center;color: white;line-height: 60px">
+            <a href="https://beian.miit.gov.cn" target="_black">Copyright© 2019-2020 版权所有 zmsnlxx 豫ICP备19042595号</a>
+        </el-footer>
     </div>
 </template>
 
@@ -34,6 +36,11 @@
             position: absolute;
             left: 0;
             bottom: 0;
+
+            a {
+                text-decoration:none;
+                color: #ffffff;
+            }
         }
 
     }
